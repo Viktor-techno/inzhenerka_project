139fbd7737848d28f4841a9e2bd7441e09765782
viktor <viktor.akimov@datadvance.net>
2024-08-29 16:16:45 +0300

    add text to script viaPyCharm

--- example.py
+++ example.py
@@ -1,6 +1,6 @@
 # Example program for Git course
 
-txt_arr = ['I', 'love', 'Git', 'so much', 'every day']
+txt_arr = ['I', 'love', 'Git', 'so much', 'every day', 'via PyCharm']
 txt = ' '.join(txt_arr) + '!'
 print('Done: ' + txt)
 print('Execution finished')
